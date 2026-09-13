30d573e60d9d8eee68a759c9f4424291a6c7aa25
Manoel <manoel.amanciio@gmail.com>
2026-09-13 16:54:38 -0300

    Adicionando validação a Tarefa.js

--- Aula 09/js/Tarefa.js
+++ Aula 09/js/Tarefa.js
@@ -1,10 +1,54 @@
-function tarefa(){
+function tarefa() {
 
     let titulo = document.getElementById('titulo').value
     let prioridade = document.getElementById('prioridade').value
     let responsavel = document.getElementById('responsavel').value
+    let descricao = document.getElementById('descricao').value
+    let dataCriacao = document.getElementById('dataCriacao').value
+    let dataConclusao = document.getElementById('dataConclusao').value
+    let status = document.getElementById('status').value
+    let projeto = document.getElementById('projeto').value
+
 
     if (titulo == '') {
+        alert('Preencha o campo obrigatório')
+        return
+    }
+
+    if (prioridade == '') {
+        alert('Preencha o campo obrigatório')
+        return
+    }
+
+    if (responsavel == '') {
+        alert('Preencha o campo obrigatório')
+        return
+    }
+
+
+    if (descricao == '') {
+        alert('Preencha o campo obrigatório')
+        return
+    }
+
+
+    if (dataCriacao == '') {
+        alert('Preencha o campo obrigatório')
+        return
+    }
+
+
+    if (dataConclusao == '') {
+        alert('Preencha o campo obrigatório')
+        return
+    }
+
+    if (status == '') {
+        alert('Preencha o campo obrigatório')
+        return
+    }
+
+    if (projeto == '') {
         alert('Preencha o campo obrigatório')
         return
     }
