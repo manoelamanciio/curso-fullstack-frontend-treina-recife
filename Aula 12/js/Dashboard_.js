20125c70898b05772096814aa3c96cde7e3e8522
Cleyton Melo <cleytonw11@gmail.com>
2026-09-22 21:24:14 -0300

    Adicionando linhas 8 e 9 na classe Dashboard.js

--- Aula 12/js/Dashboard_.js
+++ Aula 12/js/Dashboard_.js
@@ -2,9 +2,12 @@ function consultarUsuarios(){
 
     let dados = localStorage.getItem('usuarios') || '[]';
     let qtd = JSON.parse(dados);
-    let result = qtd.length;
-
+    let result = qtd.length();
     document.getElementById('qtdUsuarios').innerHTML = result
+
+    let inativos = qtd.filter(k =>k.status == 'Inativo').length()
+    document.getElementById('qtdUsuarios').innerHTML = inativos
+
 }
 
 consultarUsuarios()
