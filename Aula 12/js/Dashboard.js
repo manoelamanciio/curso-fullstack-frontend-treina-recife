0ed44eb49c77209637c994026ac413410b09ad31
Manoel <manoel.amanciio@gmail.com>
2026-09-24 10:37:24 -0300

    Criando arquivo Dashboard.jd

---
+++ Aula 12/js/Dashboard.js
@@ -0,0 +1,34 @@
+function consultarUsuarios() {
+
+    let dados = localStorage.getItem('usuarios') || '[]';
+    let qtd = JSON.parse(dados);
+    let result = qtd.length();
+    document.getElementById('qtdUsuarios').innerHTML = result
+
+    let inativos = qtd.filter(k => k.status == 'Inativo').length()
+    document.getElementById('qtdUsuarios').innerHTML = inativos
+
+}
+
+function gerarGrafico() {
+
+    const area = document.getElementById('grafico')
+    const dados = {
+        labels: [1, 2, 3, 4, 5],
+        datasets: [{
+            data: [203, 384, 199, 234, 463],
+            label: "Valores"
+        }, {
+            data: [40, 38, 19, 23, 43],
+            label: "outros"
+        }]
+    }
+
+    const myChart = new Chart(area, {
+        type: 'line',
+        data: dados
+    })
+}
+
+consultarUsuarios()
+gerarGrafico()
